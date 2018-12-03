51df5e29c93b6788c73d61a982ad1aa05934ce87
umang chouhan <umang@hotstar.com>
2018-12-03 09:10:50 +0530

    Add UICollectionView and UISearchBar
    UISearchBar to allow user to type flicker photo search query and UICollectionView to allow user to see searched images

--- UDemo/ViewController.swift
+++ UDemo/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
