97a34f62b665e9c4fc855f8874fbe3e256a1733b
umang chouhan <umang@hotstar.com>
2018-12-03 09:26:56 +0530

    Make ViewController to act as delegate of UISearchBar to initiate search of entered text

--- UDemo/ViewController.swift
+++ UDemo/ViewController.swift
@@ -9,14 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var searchBar: UISearchBar!
+    var photoQuery: PhotoQuery?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    func search(text: String) {
+        let photoQuery = PhotoQuery.init(query: text)
+        photoQuery.collectionView = self.collectionView
+        self.photoQuery = photoQuery
+    }
 }
 
+extension ViewController: UISearchBarDelegate {
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        guard let text = searchBar.text else {
+            return
+        }
+        self.search(text: text)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+}
